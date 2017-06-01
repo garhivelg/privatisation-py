22b26a2fb07e2793ba7aaf1a3b2949c11c605881
Dmitry Kutsenko <d2emonium@gmail.com>
2017-06-02 02:46:16 +0300

    src.app

--- setup.py
+++ setup.py
@@ -116,7 +116,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'server=app:run',
+            'server=src.app:run',
         ],
     },
 )
