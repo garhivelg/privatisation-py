38826e69d3bf6ce5045fc80125e9711cab6120b9
Dmitry Kutsenko <d2emonium@gmail.com>
2017-06-01 16:51:26 +0300

    Run with app

--- src/app.py
+++ src/app.py
@@ -5,7 +5,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
 
 
-def run_app():
+import os
+import logging
+
+
+from d2logger import getHandler
+
+
+def run_app(debug=False):
     app = Flask(__name__)
     app.config.from_object('config')
 
@@ -13,11 +20,34 @@ def run_app():
 
     db = SQLAlchemy(app)
     db.create_all()
+
+    handler = getHandler()
+    app.logger.addHandler(handler)
+    if debug:
+        app.logger.setLevel(logging.DEBUG)
+
+    # app.run(debug=debug)
+
     return app, db
 
 
-app, db = run_app()
+debug = os.environ.get('FLASK_DEBUG', False)
+app, db = run_app(debug=debug)
+print("Debug is %s" % (app.debug))
 
 
 from priv import *
 from case import *
+
+
+if __name__ == "__main__":
+    from priv.models.lookup import load
+    load()
+
+    app.logger.info("Starting Server")
+    app.logger.info("Debug mode is %s", app.debug)
+    from priv.models.lookup import CITIES
+    app.logger.debug("CITIES=%s", CITIES)
+
+    # app.run(debug=debug)
+    app.run()
--- src/run_server.py
+++ src/run_server.py
@@ -3,26 +3,10 @@
 from app import app
 
 
-import os
-import logging
-
-
-from d2logger import getHandler
-
-
-debug = os.environ.get('FLASK_DEBUG', False)
 if __name__ == "__main__":
-    from priv.models.lookup import load
-    load()
+    app.logger.info("Running Server")
+    app.logger.info("Debug mode is %s", app.debug)
     from priv.models.lookup import CITIES
-
-    handler = getHandler()
-    app.logger.addHandler(handler)
-    if debug:
-        app.logger.setLevel(logging.DEBUG)
-
-    app.logger.info("Starting Server")
-    app.logger.info("Debug mode is %s", debug)
     app.logger.debug("CITIES=%s", CITIES)
 
-    app.run(debug=debug)
+    app.run()
