3fede88ddab2b049c606ecd27ad715c742d2a432
Dmitry Kutsenko <d2emonium@gmail.com>
2017-06-02 02:31:13 +0300

    Path

--- setup.py
+++ setup.py
@@ -13,10 +13,10 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, '..', 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-with open(path.join(here, '..', 'requirements.txt'), encoding='utf-8') as f:
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
     requirements = f.read().splitlines()
 
 setup(
